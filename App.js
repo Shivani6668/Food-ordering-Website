0ed2922b1f15e86d47de95dc4a93f69ca28fa172
Shivani6668 <shivaniparmar316@gmail.com>
2023-11-26 05:42:21 -0800

    App is ready

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-const heading = React.createElement("h1",{class:"parent"},[
-    React.createElement("div",{class:"child"},[
-        React.createElement("h1",{},"he tag"),
-        React.createElement("h2",{},"h2 tag")
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+
+const heading = React.createElement("h1",{className:"parent"},[
+    React.createElement("div",{className:"child"},[
+        React.createElement("h1",{},"h1 tag"),
+        React.createElement("h2",{},"h2 tag shivani paemar")
     ])
 ])
 
