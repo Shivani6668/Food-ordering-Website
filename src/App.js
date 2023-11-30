ef356e802a5765e4acd345e4f2312b0dde0860d5
Shivani6668 <shivaniparmar316@gmail.com>
2023-11-30 16:03:02 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1886,12 +1886,12 @@ const Footer = () => {
     <div className="footer">
       Created By
       <i class="fa-solid fa-heart"></i>
-      <a href="https://www.linkedin.com/in/chetannada/" target="_blank">
-        Chetan Nada
+      <a href="https://www.linkedin.com/in/shivani6668/" target="_blank">
+        Shivani Parmar
       </a>
       <i class="fa-solid fa-copyright"></i>2023
       <strong>
-        Food<span>Fire</span>
+        Chef<span>Food</span>
       </strong>
     </div>
   );
