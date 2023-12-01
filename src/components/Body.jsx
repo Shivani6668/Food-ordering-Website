ef62fcc4e278c5ef416fa37d9090d335c3ea21ab
Shivani6668 <shivaniparmar316@gmail.com>
2023-12-01 17:39:16 +0530

    Search functionalty

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,111 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../utils/contants";
+import { useEffect, useState } from "react"; 
+import Shimmer from "./Shimmer"; 
+import { swiggy_api_URL } from "../utils/contants";
 
+// Body Component for body section: It contain all restaurant cards
 const Body = () => {
-    return (
-      <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
-          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-        })}
-      </div>
-    );
+  // useState: To create a state variable, searchText, allRestaurants and filteredRestaurants is local state variable
+  const [searchText, setSearchText] = useState("");
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    // handle the error using try... catch
+    try {
+      const response = await fetch(swiggy_api_URL);
+      const json = await response.json();
+
+      // initialize checkJsonData() function to check Swiggy Restaurant data
+      async function checkJsonData(jsonData) {
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+
+          // initialize checkData for Swiggy Restaurant data
+          let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+          // if checkData is not undefined then return it
+          if (checkData !== undefined) {
+            return checkData;
+          }
+        }
+      }
+
+      // call the checkJsonData() function which return Swiggy Restaurant data
+      const resData = await checkJsonData(json);
+
+      // update the state variable restaurants with Swiggy API data
+      setAllRestaurants(resData);
+      setFilteredRestaurants(resData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+function filterData(searchText, restaurants) {
+  const resFilterData = restaurants.filter((restaurant) =>
+    restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return resFilterData;
+}
+
+
+  const searchData = (searchText, restaurants) => {
+    if (searchText !== "") {
+      const filteredData = filterData(searchText, restaurants);
+      setFilteredRestaurants(filteredData);
+      setErrorMessage("");
+      if (filteredData?.length === 0) {
+        setErrorMessage("No matches restaurant found");
+      }
+    } else {
+      setErrorMessage("");
+      setFilteredRestaurants(restaurants);
+    }
   };
 
-  export default Body
+  if (!allRestaurants) return null;
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search a restaurant you want..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          onKeyDown={()=> searchData(searchText, allRestaurants)}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            searchData(searchText, allRestaurants);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      {errorMessage && <div className="error-container">{errorMessage}</div>}
+
+      {allRestaurants?.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <div className="restaurant-list">
+          {filteredRestaurants.map((restaurant) => {
+            return (
+              <RestaurantCard key={restaurant?.info?.id} {...restaurant?.info} />
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Body;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,38 @@
-import Logo from "../Images/Logo.jpg"
+import { useState } from "react";
+import Logo from "../Images/Logo.jpg";
 
 const Title = () => (
-    <a href="/">
-      <img className="logo" src={Logo} alt="Logo" />
-    </a>
-  );
-  
-  const Header = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li><i class="fa-solid fa-cart-shopping"></i></li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  <a href="/">
+    <img className="logo" src={Logo} alt="Logo" />
+  </a>
+);
 
-  export default Header
+const Header = () => {
+  const [login, setLogin] = useState("Login");
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <button
+            onClick={() => {
+              login === "Login" ? setLogin("LogOut") : setLogin("Login");
+            }}
+            className="login"
+          >
+            <li>{login}</li>
+          </button>
+          <li>
+            <i class="fa-solid fa-cart-shopping"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
